9f3564fe6a0d833a8149ebc32cd92b01e9454ba3
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-02-17 10:18:13 -0300

    change path coverage

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/local-weather-app'),
+      dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }],
     },
